3d5b565dadc8acc4ca4197210e369c9cb9158065
leroylsy <18149052617@163.com>
2019-06-18 09:21:16 +0800

    默认封装obtain_jwt_token实现JWT校验及token返回值函数重写

---
+++ apps/admin/utils/jwt_response.py
@@ -0,0 +1,24 @@
+# 自定义返回值，默认只返回token，需要重新函数
+# username
+# password
+# token
+def jwt_response_payload_handler(token, user=None, request=None):
+    """
+    Returns the response data for both the login and refresh views.
+    Override to return a custom response such as including the
+    serialized representation of the User.
+
+    Example:
+
+    def jwt_response_payload_handler(token, user=None, request=None):
+        return {
+            'token': token,
+            'user': UserSerializer(user, context={'request': request}).data
+        }
+
+    """
+    return {
+        'token': token,
+        'username': user.username,
+        'user_id': user.id
+    }
